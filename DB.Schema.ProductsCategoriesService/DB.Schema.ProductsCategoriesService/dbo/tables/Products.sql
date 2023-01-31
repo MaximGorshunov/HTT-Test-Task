162b89a521b393b47c1a279ff49d15586ffefe73
Maxim Gorshunov <maximgorshunov@gmail.com>
2023-01-31 15:40:50 +0300

    Add db schema

---
+++ DB.Schema.ProductsCategoriesService/DB.Schema.ProductsCategoriesService/dbo/tables/Products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL,
+	[Amount] INT NOT NULL, 
+    [Price] DECIMAL NOT NULL,  
+	[CategoryId] INT NOT NULL, 
+    CONSTRAINT [FK_Products_ToCategories] FOREIGN KEY ([CategoryId]) REFERENCES [Categories]([Id])
+)
